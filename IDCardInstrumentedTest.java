f57c35429367e93978b316ec8eb809ab27daae59
zihan77880318-coder <zihan77880318@gmail.com>
2026-01-08 15:38:58 +0800

    Create IDCardInstrumentedTest.java

---
+++ IDCardInstrumentedTest.java
@@ -0,0 +1,22 @@
+package top.olws.idcarddetect;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import androidx.test.espresso.action.ViewActions;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class IDCardInstrumentedTest {
+    @Test
+    public void testRecognizeIDCard() {
+//        onView(withId(R.id.btnCapture)).perform(ViewActions.click());
+//        boolean result = idCardI.recognizeIDCard();
+//        assertEquals(true, result);
+    }
+}
